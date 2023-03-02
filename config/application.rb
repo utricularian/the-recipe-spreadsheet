e086224ad690d943c715fa7a5e965429ec809f2f
Sam Serrano <samuel.j.serrano@gmail.com>
2023-03-02 13:50:25 -0800

    Boilerplate, rubocop, and rspec

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,15 @@ module TheRecipeSpreadsheet
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |gen|
+      gen.test_framework :rspec,
+                         fixtures: false,
+                         view_specs: false,
+                         helper_specs: false,
+                         routing_specs: false,
+                         request_specs: false,
+                         controller_specs: false
+    end
   end
 end
