64e6c376fab1f585e2d312f92a8409e1ff6bd6eb
Sam Serrano <samuel.j.serrano@gmail.com>
2023-03-15 09:28:48 -0700

    Backfilling tests for user flow (test are broken)

--- app/controllers/v1/profiles_controller.rb
+++ app/controllers/v1/profiles_controller.rb
@@ -1,9 +1,9 @@
 module V1
   class ProfilesController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, only: [:show]
 
     def show
-      render json: current_user.to_json
+      render json: current_user.to_json(only: [:id, :email])
     end
 
     def update; end
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,127 @@
+require 'rails_helper'
+
+RSpec.describe 'User API' do
+  describe 'POST /api/v1/users.json' do
+    let(:draft) { build(:user) }
+
+    def make_request(email: nil, password: nil)
+      email ||= draft.email
+      password ||= draft.password
+      post '/api/v1/users.json', params: { user: { email:, password:, password_confirmation: password }}
+    end
+
+    it 'returns auth headers' do
+      make_request
+
+      expect(response).to be_created
+      expect(response.headers['Authorization']).not_to be_nil
+
+      json = response.parsed_body
+      expect(json['id']).to_not be_nil
+      expect(json['email']).to eq(draft.email)
+
+      expect(json.keys).to match_array(['id', 'email'])
+    end
+
+    context 'with incorrect credentials' do
+      it 'returns an error' do
+        make_request(password: 'foobar')
+
+        expect(response).to be_unprocessable
+        expect(response.headers['Authorization']).to be_nil
+
+        json = response.parsed_body
+        expect(json).to eq({ 'errors' => { 'password' => ['is too short (minimum is 12 characters)'] } })
+      end
+    end
+  end
+
+  describe 'POST /api/v1/users/sign_in.json' do
+    let(:user) { create(:user) }
+
+    def make_request(email: nil, password: nil)
+      email ||= user.email
+      password ||= user.password
+      post '/api/v1/users/sign_in.json', params: { user: { email:, password: } }
+    end
+
+    it 'returns auth headers' do
+      make_request
+
+      expect(response).to be_created
+      expect(response.headers['Authorization']).not_to be_nil
+
+      json = response.parsed_body
+      expect(json).to eq({
+                           'id' => user.id,
+                           'email' => user.email
+                         })
+    end
+
+    context 'with incorrect credentials' do
+      it 'returns an error' do
+        make_request(password: 'foobar')
+
+        expect(response).to be_unauthorized
+        expect(response.headers['Authorization']).to be_nil
+
+        json = response.parsed_body
+        expect(json).to eq({ 'error' => 'Invalid Email or password.' })
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/users/sign_out.json' do
+    let(:user) { create(:user) }
+
+    def make_request
+      delete '/api/v1/users/sign_out.json'
+    end
+
+    before do
+      sign_in user
+    end
+
+    it 'invalidates the session' do
+      get '/api/v1/profile.json'
+      expect(response).to be_ok
+
+      make_request
+      expect(response.parsed_body).to be_empty
+
+      get '/api/v1/profile.json'
+      expect(response).to be_unauthorized
+    end
+  end
+
+  describe 'GET /api/v1/profile.json' do
+    def make_request
+      get '/api/v1/profile.json'
+    end
+
+    it 'requires authentication' do
+      make_request
+
+      expect(response).to be_unauthorized
+    end
+
+    context 'when logged in' do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in user
+      end
+
+      it 'returns logged in user info' do
+        make_request
+
+        expect(response).to be_ok
+        json = response.parsed_body
+        expect(json).to eq({
+                             'id' => user.id,
+                             'email' => user.email
+                           })
+      end
+    end
+  end
+end
